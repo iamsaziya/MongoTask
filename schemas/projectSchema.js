ec02a3979fac126c6c8028a8177b9b92603caf64
imshn <shaan@vyomr.in>
2024-11-10 00:06:17 +0530

    Validation, schemas ready for datas: users, tasks, projects

--- schemas/projectSchema.js
+++ schemas/projectSchema.js
@@ -2,11 +2,11 @@ const projectSchema = {
   projectId: { type: 'string', required: true },
   name: { type: 'string', required: true },
   description: { type: 'string' },
-  createdAt: { type: 'string' },
+  createdAt: { type: 'object' },
   createdBy: { type: 'string', required: true },
   active: { type: 'boolean', required: true },
   tasks: { type: 'object', required: true },
-  deadline: { type: 'string', required: true },
+  deadline: { type: 'object', required: true },
   status: {
     type: 'string',
     required: true,
--- schemas/taskSchema.js
+++ schemas/taskSchema.js
@@ -1,13 +1,13 @@
 const taskSchema = {
-  taskId: { type: 'string', required: true },
-  projectId: { type: 'string', required: true },
-  name: { type: 'string', required: true },
-  description: { type: 'string' },
-  priority: { type: 'string', enum: ["LOW", "MEDIUM", "HIGH"] },
-  deadline: { type: 'string', required: true },
-  createdBy: { type: 'string', required: true },
-  createdAt: { type: 'string' },
-  status: { type: 'string', enum: ["PENDING", "INPROGRESS", "COMPLETED"] }
+  taskId: { type: "string", required: true },
+  projectId: { type: "string", required: true },
+  name: { type: "string", required: true },
+  description: { type: "string" },
+  priority: { type: "string", enum: ["LOW", "MEDIUM", "HIGH"] },
+  deadline: { type: "object", required: true },
+  createdBy: { type: "string", required: true },
+  createdAt: { type: "object" },
+  status: { type: "string", enum: ["PENDING", "INPROGRESS", "COMPLETED"] }
 };
 
 module.exports = taskSchema;
