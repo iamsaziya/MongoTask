89f4b6bf91e695d382c20a09744ca5636a9bdd23
imshn <shaan@vyomr.in>
2024-11-19 13:08:25 +0530

    Project GET API and CREATE API COMPLETED

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,16 +1,35 @@
-const { saveProjectToDb } = require("../lib/db.js");
+const { saveProjectToDb, getProjectById } = require("../lib/db.js");
 const saveProject = async (req, res) => {
   const project = req.body;
   try {
-    await saveProjectToDb(project);
-    console.log("Project saved successfully");
-    res
-      .status(201)
-      .json({ success: true, message: "Project saved successfully" });
+    const resFromController = await saveProjectToDb(project);
+    if (resFromController.name) {
+      res
+        .status(201)
+        .json({ success: true, message: "Project saved successfully" });
+    } else {
+      res.status(400).json({ success: false, message: resFromController });
+    }
   } catch (err) {
-    res.status(500).json({ success: false, message: "Error saving project" });
+    res.status(500).json({ success: false, message: err.message });
     console.error("Error saving project:", err.message);
   }
 };
 
-module.exports = { saveProject };
+const getProjectByProjectId = async (req, res) => {
+  const projectId = req.params.projectId;
+  try {
+    const projectData = await getProjectById(projectId);
+    console.log("Project data:", projectData);
+    if (projectData.length > 0) {
+      res.status(200).json({ success: true, data: projectData });
+    } else {
+      res.status(404).json({ success: false, message: "Project not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+    console.error("Error fetching project:", err.message);
+  }
+} 
+
+module.exports = { saveProject, getProjectByProjectId };
